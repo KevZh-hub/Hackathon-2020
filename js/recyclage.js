95e1323a9764dae5f7a650b91d3349a0f3cc1d11
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-10-15 14:59:33 +0200

    ADD Produits

--- js/recyclage.js
+++ js/recyclage.js
@@ -1,13 +1,12 @@
-// const nbMegots = Math.floor(Math.random() * 1000);
-// const domNumber = document.getElementById('nbMegot');
 
-// function updateNbMegot(){
-//     while(domNumber != nbMegots){
-//         domNumber.innerHTML = 
-//     }
-// }
+const nbMegots = Math.floor(Math.random() * 1000);
+const nbProduits = Math.floor(nbMegots / 8);
+const listProduits = [
+    'couvertures',
+    'vestes',
+    'manteaux'
 
-
+];
 
 function animateValue(id, start, end, duration) {
     if (start === end) return;
@@ -15,17 +14,26 @@ function animateValue(id, start, end, duration) {
     var current = start;
     var increment = end > start? 1 : -1;
     var stepTime = Math.abs(Math.floor(duration / range));
-    console.log(stepTime);
     var obj = document.getElementById(id);
     var timer = setInterval(function() {
         current += increment;
         obj.innerHTML = current;
         if (current == end) {
             clearInterval(timer);
+            showProduct();
+
         }
     }, stepTime);
 }
 
+
+function showProduct(){
+    document.getElementById('nbProduit').innerHTML = nbProduits;
+    console.log(listProduits[Math.floor(Math.random() * 3)]);
+    document.getElementById('produit').innerHTML = listProduits[Math.floor(Math.random() * 3)];
+}
+
 window.onload = function() {
-    animateValue("nbMegots", 0, Math.floor(Math.random() * 501), 8000);
+    animateValue("nbMegots", 0, nbMegots, 8000);
 };
+
